2410046bf06dac06d5bdd780377163388c796f75
Daniel Mazour <dmazour@Daniels-MBP.home>
2015-06-17 09:46:59 -0400

    first commit

---
+++ GimbalFeed/ImageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ImageViewController.h
+//  GimbalFeed
+//
+//  Created by Daniel Mazour on 6/16/15.
+//  Copyright (c) 2015 Daniel Mazour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@end
